0eaea3047d1c7c26b950185cf815717a497a6534
Bean <ting-yi.wang@ucl.ac.uk>
2018-05-18 16:34:35 +0100

    add Adjacency_Matrix in mesh.h

--- IGLFramework/include/mesh.h
+++ IGLFramework/include/mesh.h
@@ -8,6 +8,7 @@
 #include <iostream>
 
 #include <Eigen/Dense>
+#include <Eigen/Sparse>
 using namespace Eigen;
 using namespace std;
 
@@ -15,9 +16,10 @@ using namespace std;
 class mesh {
     public:
     tuple<Matrix3d, Vector3d, double> ICP(MatrixXd Pv, MatrixXd Qv, int step_size);
+    SparseMatrix<double> Adjacency_Matrix(MatrixXi F);
     MatrixXd Add_noise(MatrixXd m, double noise_val);
     pair<MatrixXd, MatrixXd> rotate(MatrixXd, double x, double y, double z);
-    MatrixXd non_rigid_ICP(MatrixXd Temp_V, MatrixXd Temp_F, MatrixXd Target_V, MatrixXi Target_F);
+    MatrixXd non_rigid_ICP(MatrixXd Temp_V, MatrixXi Temp_F, MatrixXd Target_V, MatrixXi Target_F);
 };
 #endif
 
